f0759b986989d1018e2b8a63707705a0babdcb0d
Ayush Dekhane <AyushDekhane@gmail.com>
2025-11-02 21:20:03 +0530

    Update asset URLs and change to production server

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://weshare-7d3y.onrender.com'
 export const currency = '₹'
 
 export const assets ={
@@ -16,4 +16,5 @@ export const assets ={
     upload_area,
     parcel_icon
 }
+
 
